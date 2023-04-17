3ccda29cf6fba62568d9f80f5200bb140c3c178a
kunal jangle <108586012+kunalj12@users.noreply.github.com>
2023-04-17 13:41:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const firstName = document.getElementById("firstName").value;
+const lastName = document.getElementById("lastName").value;
+const phoneNumber = document.getElementById("phoneNumber").value;
+const email = document.getElementById("email").value;
+
+const result = `Name: ${firstName} ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`;
+
+alert(result);
